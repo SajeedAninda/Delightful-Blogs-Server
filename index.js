73b43b97aefc506952139b26012ba11a60f55743
SajeedAninda <sajaninda@gmail.com>
2023-11-05 12:27:55 +0600

    Created Blogs DB Collection

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ async function run() {
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // DATABASE COLLECTIONS 
+        let blogsCollection = client.db("delightfulBlogsDB").collection("blogs");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
