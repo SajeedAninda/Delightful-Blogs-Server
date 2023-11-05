87457b6c1417b0a046483afe9274f6e5800cfbd6
SajeedAninda <sajaninda@gmail.com>
2023-11-05 21:00:52 +0600

    Minor Changes to the api

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
 
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ruhvmdy.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -97,7 +96,7 @@ async function run() {
         });
 
         // API TO GET SINGLE DATA BY ID FOR BLOG DETAILS 
-        app.get("/allBlogs/:id", async (req, res) => {
+        app.get("/blogDetails/:id", async (req, res) => {
             const id = req.params.id;
             const query = {
                 _id: new ObjectId(id),
